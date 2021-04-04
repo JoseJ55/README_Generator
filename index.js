fa4525a902c657a354f04e4b9d4f15658eba7a8c
Jose Jimenez <josejimenez826@gmail.com>
2021-04-03 21:07:37 -0700

    added basic code

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require("inquirer");
+const stringGen = require('./string');
+
+const {stringGenerator} = stringGen;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: `input`,
+    name: `petname`,
+    message: 'What is pet name'
+},
+{
+    type: `input`,
+    name: `petage`,
+    message: 'What is pet age'
+}];
+
+const init = () =>{
+    inquirer.prompt(questions).then((answers) => {
+        const {petname, petage} = answers;
+
+        fs.writeFile('read.txt', stringGenerator(petname, petage), (err)=>{
+            if (err) throw err;
+            console.log("The file has been saved!");
+        })
+    })
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
 init();
---
+++ string.js
@@ -0,0 +1,7 @@
+const stringGenerator = (petName, petage) =>{
+    return `my pet name is ${petName} and age is ${petage}.\n`
+}
+
+module.exports = {
+    stringGenerator
+}
